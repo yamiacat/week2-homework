c915880756c686b0ae66b3ded6e05aadd612e1de
Matt Rose <yamiacat@gmail.com>
2017-04-17 17:30:34 +0100

    Part B complete

--- day_01_lab_homework/student_class_homework_part_b.rb
+++ day_01_lab_homework/student_class_homework_part_b.rb
@@ -1,17 +1,35 @@
 class SportsTeam
 
-   attr_accessor :team_name, :players, :coach
+   attr_accessor :team_name, :players, :coach, :points
 
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = 0
 
   end
 
 
   def replace_coach(new_coach)
     @coach = new_coach
+  end
+
+  def add_player(new_player)
+    @players << new_player
+  end
+
+  def check_player_is_in_team(name_to_check)
+    for team_mate in @players
+      return true if name_to_check == team_mate
+    end
+    return "They're not on the team."
+  end
+
+  def update_score(match_result)
+    @points += 1 if match_result == "win"
+    @points -= 1 if match_result == "lose"
+    @points += 0 if match_result == "draw"
   end
 
 end
--- day_01_lab_homework/student_class_homework_part_b_spec.rb
+++ day_01_lab_homework/student_class_homework_part_b_spec.rb
@@ -36,4 +36,43 @@ class ClassHomeworkTest < MiniTest::Test
     assert_equal("Ripley", @team_2.replace_coach("Ripley"))
   end
 
+  def test_add_new_player_by_array_length
+    @team_1.add_player("Polymorph")
+    player_count = @team_1.players.count
+    assert_equal(5, player_count)
+  end
+
+  def test_add_new_player_by_array_contents
+    assert_equal(["Hicks", "Hudson", "Vasquez", "Drake", "Bishop"], @team_2.add_player("Bishop"))
+  end
+
+  def test_check_player_is_in_team
+    assert_equal(true, @team_1.check_player_is_in_team("Lister"))
+  end
+
+  def test_check_player_is_not_in_team
+    assert_equal("They're not on the team.", @team_1.check_player_is_in_team("Hollister"))
+  end
+
+  def test_points_are_zero
+    assert_equal(0, @team_1.points)
+  end
+
+  def test_points_changes_with_win
+    @team_2.update_score("win")
+    assert_equal(1, @team_2.points)
+  end
+
+  def test_points_changes_with_loss
+    @team_1.update_score("lose")
+    assert_equal(-1, @team_1.points)
+  end
+
+
+  def test_points_changes_with_draw
+    @team_2.update_score("draw")
+    assert_equal(0, @team_2.points)
+  end
+
+
 end
