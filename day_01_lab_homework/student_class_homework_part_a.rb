e88c4047710ddaebcd5d82ed93eeabd0cbcbbf62
Matt Rose <yamiacat@gmail.com>
2017-04-17 16:09:54 +0100

    Getters and Setters completed for part A

--- day_01_lab_homework/student_class_homework_part_a.rb
+++ day_01_lab_homework/student_class_homework_part_a.rb
@@ -1,15 +1,25 @@
 class Student
 
-def initialize(name, cohort)
-  @name = name
-  @cohort = cohort
-end
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
 
+  def get_student_name
+    return @name
+  end
 
+  def get_student_cohort
+    return @cohort
+  end
 
+  def rename_student(new_name)
+    @name = new_name
+  end
 
-
-
+  def set_student_cohort(new_cohort)
+    @cohort = new_cohort
+  end
 
 
 end
--- day_01_lab_homework/student_class_homework_part_a_spec.rb
+++ day_01_lab_homework/student_class_homework_part_a_spec.rb
@@ -6,18 +6,43 @@ class ClassHomeworkTest < MiniTest::Test
 
   def setup
 
-
+    @student_1 = Student.new("Socrates", 1)
+    @student_2 = Student.new("Plato", 2)
 
   end
 
-  def test_student_class_has_name
-    assert_same(Student)
+
+  def test_student_class_exists
+    assert(@student_1)
+  end
+
+  def test_student_class_is_a_class
+    assert_equal(Student, @student_1.class)
+  end
+
+  def test_student_name_getter
+    assert_equal("Socrates", @student_1.get_student_name)
   end
 
 
+  def test_student_name_getter_not_hardcoded
+    assert_equal("Plato", @student_2.get_student_name)
+  end
 
+  def test_student_cohort_getter
+    assert_equal(1, @student_1.get_student_cohort)
+  end
 
+  def test_student_cohort_getter_not_hardcoded
+    assert_equal(2, @student_2.get_student_cohort)
+  end
 
+  def test_student_name_setter
+    assert_equal("Socrates Rex", @student_1.rename_student("Socrates Rex"))
+  end
 
+  def test_student_cohort_setter
+    assert_equal(2, @student_1.set_student_cohort(2))
+  end
 
 end
